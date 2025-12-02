d941adfbabbe583c0ed9e1c9fae392dfb9d03800
Mesh <meshmwangi828@gmail.com>
2025-12-02 19:43:02 +0300

    Fi

--- fusion_force/urls.py
+++ fusion_force/urls.py
@@ -13,5 +13,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# Custom 404 handler
-handler404 = 'main.views.page_not_found'
+
